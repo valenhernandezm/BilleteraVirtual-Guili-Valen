307b5e3ffa3728d45fa10f77dd961fd1777f07b9
valentina <valentina@localhost>
2016-05-11 17:30:26 -0430

    Proceso implementacion Existe Clase BilleteraElectronica

---
+++ tarea3/Pruebas.py
@@ -0,0 +1,11 @@
+'''
+Created on May 11, 2016
+
+@author: valentina
+'''
+import unittest
+from BilleteraElectronica import *
+
+class BilleteraTest(unittest.TestCase):
+    def testBilleteraElectronicaExiste(self):
+        BilleteraElectronica()
